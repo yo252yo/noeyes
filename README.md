fed931d3546aaf752eaf0cdff0a2ee2d3eb2a6d4
yo252yo <yo252yo@gmail.com>
2026-01-03 01:30:50 +0100

    leave things in a good state to be discovered

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 Concept:
 <img width="935" height="600" alt="image" src="https://github.com/user-attachments/assets/44322d99-ce00-48e4-a1d6-ad3ab732d412" />
+
+icons: https://win98icons.alexmeub.com/
--- resource/script.js
+++ resource/script.js
@@ -4,15 +4,15 @@ console.log('script');
 window.addEventListener('message', (event) => {
     if (event.data.type === 'chatters') {
         if (window.location.pathname.includes('diary.html')) {
-            // Display chatters in diary.html
-            let chatterDiv = document.getElementById('chatters-list');
-            if (!chatterDiv) {
-                chatterDiv = document.createElement('div');
-                chatterDiv.id = 'chatters-list';
-                document.body.appendChild(chatterDiv);
-            }
-            const list = event.data.data.join(', ');
-            chatterDiv.innerHTML = '<br><br>Chatters: ' + list;
+            // // Display chatters in diary.html
+            // let chatterDiv = document.getElementById('chatters-list');
+            // if (!chatterDiv) {
+            //     chatterDiv = document.createElement('div');
+            //     chatterDiv.id = 'chatters-list';
+            //     document.body.appendChild(chatterDiv);
+            // }
+            // const list = event.data.data.join(', ');
+            // chatterDiv.innerHTML = '<br><br>Chatters: ' + list;
         } else {
             // Forward to child iframes
             const iframes = document.querySelectorAll('iframe');
